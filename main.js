63e4389dfaf3fd51382b32572293a5f89dec8a29
Itachi-MacOS <warrior.006@inbox.ru>
2024-03-25 15:42:15 +0500

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('Hello world')
+const {crawlPage} = require('./crawl.js')
+
+function main () {
+  if (process.argv.length < 3) {
+    console.error('no website provided')
+    process.exit(1)
+  }
+  if (process.argv.length > 3) {
+    console.error('too many cli args')
+    process.exit(1)
+  }
+  for (const arg of process.argv) {
+    console.log(arg)
+  }
+  const baseURL = process.argv[2]
+  console.log('starting crawl of ' + baseURL)
+  crawlPage(baseURL)
+}
+
+main()
